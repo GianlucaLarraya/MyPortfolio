26f674ced5be2f51c55265a2a12b7d8791e48d69
Gianluca Larraya <gianluca.larraya@gmail.com>
2023-02-03 11:38:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import project1 from "./assets/myowntwitterimage.png"
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>GIANLUCA LARRAYA</h1>
+      <div className='trabajostitulo'>My work</div>
+      <div className='line'></div>
+      <div className='trabajos'>
+      <div className="card-glass">
+        <img src={project1}></img>
+         <p className="cardtitle">My own Twitter</p>
+         <p className="cardtext">Application that tries to imitate Twitter Web App. You can tweet, retweet and like tweets and follow and unfollow people. </p>
+      </div>
+      <div className="card-glass">Proyect</div>
+      <div className="card-glass">Proyect</div>
+      <div className="card-glass">Proyect</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
